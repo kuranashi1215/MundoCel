1f689b505ead501b13bd9c462b0379ba4764f91b
kura <macastilla592@misena.edu.co>
2022-06-14 11:49:55 -0500

    se soluciono error pequeño

--- controller/template.controller.php
+++ controller/template.controller.php
@@ -7,7 +7,7 @@ class Template{
         if (isset($_SESSION['login']) and $_SESSION['login'] == true ){
             require_once "view/template.php";
         }else{
-            require_once "view/module/login.php";
+            // require_once "view/module/login.php";
         }
 
     }
