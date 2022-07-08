c9c8a90f19bfa1ae7b961d32277a68918c5384b4
Pimbo <Pimboto@hotmail.com>
2022-07-07 19:06:05 -0500

    Se agrega carrito de compras

--- view/module/products.php
+++ view/module/products.php
@@ -1,3 +1,8 @@
+<?php 
+$productos = obtenerProductos();
+?>
+<?php foreach ($productos as $producto) {  ?>
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
@@ -8,6 +13,8 @@
 require 'model/config.php';
 $db = new Database();
 $con = $db ->conectar();
+
+
 
 
 
@@ -50,8 +57,24 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                             <button type="button" class="btn btn-primary mt-5" data-toggle="collapse" data-target="#demo">Leer mas</button>
                             <br>
                             
-                            <a href="" class="btn btn-success" onclick="addProducto(<?php echo $id;  ?>, <?php echo $token_tmp ?>)">Agregar</a>
-                         
+                            <?php if (productoYaEstaEnCarrito($producto->id)) { ?>
+                        <form action="funciones/eliminar_del_carrito.php" method="post">
+                            <input type="hidden" name="id_producto" value="<?php echo $producto->id ?>">
+                            <span class="button is-success">
+                                <i class="fa fa-check"></i>&nbsp;En el carrito
+                            </span>
+                            <button class="button is-danger">
+                                <i class="fa fa-trash-o"></i>&nbsp;Quitar
+                            </button>
+                        </form>
+                    <?php } else { ?>
+                        <form action="funciones/agregar_al_carrito.php" method="post">
+                            <input type="hidden" name="id_producto" value="<?php echo $producto->id ?>">
+                            <button class="button is-primary">
+                                <i class="fa fa-cart-plus"></i>&nbsp;Agregar al carrito
+                            </button>
+                        </form>
+                    <?php } ?>
                                 <h6><?php echo $row['descripcion'] ?></h6>
                                 <!-- <a href="shopdetails" class="add-cart">Detalles</a> -->
                                 <h5><?php echo number_format($row['precio'], 2, '.', ',');?></h5>
@@ -62,5 +85,6 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
                 <?php }?>  
             </div> 
         </div>
+        <?php } ?>
     </section>
     <!-- Termina Seccion Productos -->
