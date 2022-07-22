673aa107fb36ce657f65f65ee9910e483d9b1302
kura <macastilla592@misena.edu.co>
2022-07-21 20:06:03 -0500

    agrego todo lo de master aqui por si las dudas

--- index.php
+++ index.php
@@ -23,14 +23,13 @@
     /////////////////////////////////////////////
     require_once 'controller/user.controller.php';
     require_once 'controller/template.controller.php';
-    require_once 'controller/ControllerProductos.php';
+   
     /////////////////////////////////////////////
     //                   MODEL                 //
     /////////////////////////////////////////////
     require_once 'model/dao/user.dao.php';
     require_once 'model/dto/user.dto.php';
-    require_once 'model/ModelProductos.php';
-    
+
     
     /////////////////////////////////////////////
     //                 CONEXION                //
@@ -39,19 +38,13 @@
     require 'model/config.php';
     
     /////////////////////////////////////////////
-    //                  LIBRARYS               //
-    /////////////////////////////////////////////
-    
-    
-    
-    /////////////////////////////////////////////
-    //          funciones carrito              //
+    //           FUNCIONES CARRITO             //
     /////////////////////////////////////////////
     require_once 'funciones/funciones.php';
     
-
     /////////////////////////////////////////////
-                    /* run */
+    //                    RUN                  //
+    /////////////////////////////////////////////
     $objRun = new Template();
     $objRun->getIntro();
     
